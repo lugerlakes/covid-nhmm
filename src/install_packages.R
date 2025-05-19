d84fec1834335f69fdbbd9910d284db012acf1f7
lugerlakes <fernando.lagos.alvarado@gmail.com>
2025-05-18 23:51:20 -0400

    visualizations created

--- src/install_packages.R
+++ src/install_packages.R
@@ -1,7 +1,9 @@
 # install_packages.R
 # Script to install required R packages for COVID-nHMM-Chile project
 
-required_packages <- c("depmixS4", "lme4", "glmnet", "ggplot2", "data.table", "localconverter","httpgd","base","utils","hrbrthemes","dplyr","tidyr","ggthemes","ggpubr","ggsci","viridis","sf", "MuMIn")
+required_packages <- c("depmixS4", "lme4", "glmnet", "ggplot2", "data.table", "localconverter",
+                       "httpgd", "base", "utils", "hrbrthemes", "dplyr", "tidyr", "ggthemes", 
+                       "ggpubr", "ggsci", "viridis", "sf", "MuMIn", "showtext", "extrafont")  # Agregado showtext y extrafont
 
 # Set CRAN mirror (cloud-based, stable)
 options(repos = c(CRAN = "https://cloud.r-project.org"))
@@ -16,3 +18,7 @@ install_if_missing <- function(pkg) {
 }
 
 invisible(sapply(required_packages, install_if_missing))
+
+# Cargar showtext automáticamente para asegurar fuentes en gráficos ggplot2
+library(showtext)
+showtext_auto()
